8a57d95e50d040fa13b6e232d198e7c4d5ffb02d
Palina <palina.paliakova@gmail.com>
2022-10-18 18:02:30 +0200

    Added new functionalities: counter of tries, beginning of new game if the user wants and the opportunity to set right border

--- random/randomnumbergenerator.py
+++ random/randomnumbergenerator.py
@@ -1,26 +1,42 @@
-import random # импортируем модуль
-num1 = random.randint(1, 100) # генерируем случайное число в промежутке от 1 до 100
+import random 
 print("Добро пожаловать в числовую угадайку")
+print("Введите правую границу диапозона")
+n = int(input())
 
-def is_valid(num): #  проверяем, чтобы пользователь ввел правильное число
+ 
+num1 = random.randint(1, n) 
+
+
+
+def is_valid(num): 
     
     if num.isdigit():
-        if 1 <= int(num) <= 100:
+        if 1 <= int(num) <= n:
             return True
     return False
 
+counter = 1
 while True:
      num = input()
+     
      if  is_valid(num) == False:
-         print("А может быть все-таки введем целое число от 1 до 100?")
-         continue # при неправильном вводе просим снова попытаться ввести число от 1 до 100
+         print("А может быть все-таки введем целое число от 1 до границы вы указали?")
+         counter += 1
+         continue 
      else:
-         num = int(num) # переводим вводимое число в int
+         num = int(num) 
      if num > num1:
+         counter += 1
          print("Ваше число больше загаданного, попробуйте еще разок")
      elif num < num1:
+         counter += 1
          print("Ваше число меньше загаданного, попробуйте еще разок")
      else:
-         print("Вы угадали, поздравляем!")
-         break  # завершаем цикл, когда число было угадано
+         print("Вы угадали, поздравляем!","Количество попыток :", counter)
+         print("Хотите сыграть еще раз? Да или Нет?")
+         responce = input().lower()
+         if responce == "да":
+            num1 = random.randint(1, n)
+         else:     
+             break  
 print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
